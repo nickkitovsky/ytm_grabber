fe0239121272cb4a63a17db9e043b3610af0f028
nickkitovsky <1825545+nickkitovsky@users.noreply.github.com>
2024-01-16 23:42:58 +0700

    refactoring authdata

--- ytm_grabber/core/authdata.py
+++ ytm_grabber/core/authdata.py
@@ -1,7 +1,6 @@
 """Module for working with auth files."""
 import json
 import shlex
-from dataclasses import dataclass
 from pathlib import Path
 from typing import Self
 from urllib.parse import parse_qsl, urlsplit
@@ -11,77 +10,21 @@ import pyperclip  # type: ignore[import-untyped]
 from ytm_grabber.core.custom_exceptions import DumpAuthFileError, ParsingError
 
 
-@dataclass
 class AuthData:
     """Data for authentification on Youtube Music."""
 
-    headers: dict
-    params: dict
-    json_data: dict
-
-
-class AuthDataParser:
-    """Parser for authdata from file or clipboard."""
-
     def __init__(self, raw_curl_content: list[str]) -> None:
-        self._raw_curl_content = raw_curl_content
-        if "^" in self._raw_curl_content[0]:
-            self._raw_curl_content = self._fix_cmd_break_lines(raw_curl_content)
+        self.headers: dict = {}
+        self.params: dict = {}
+        self.json_data: dict = {}
+        self.raw_curl_content = raw_curl_content
+        if "^" in self.raw_curl_content[0]:
+            self.raw_curl_content = self._fix_cmd_break_lines(content_with_cr=raw_curl_content)
         try:
-            self.authdata = self.parse_authdata()
+            self._parse_authdata()
         except (IndexError, json.JSONDecodeError) as exc:
             msg = "error of parse curl_data"
             raise ParsingError(msg) from exc
-
-    def parse_authdata(self) -> AuthData:
-        """Parse content of curl file to AuthData(headers, data, params) dataclass.
-
-        Args:
-        ----
-            content (list[str]): content of file 'copy as cURL' from browser
-
-        Returns:
-        -------
-            AuthData: dataclass for store data for authentification on Youtube Music
-        """
-        # Parsing 'params' section
-        post_url = shlex.split(self._raw_curl_content[0])[1]
-        query_post_url = urlsplit(post_url).query
-        post_params = dict(parse_qsl(query_post_url))
-        # Parsing 'data' section
-        raw_data = shlex.split(s=self._raw_curl_content[-2])[1]
-        json_data = json.loads(s=raw_data)
-        # delete default browse id
-        json_data.pop("browseId", None)
-        # Parsing 'headers' section
-        raw_headers = self._raw_curl_content[1:-2]
-        # Parse str  "-H 'key: value' \\\n" to "key: value"
-        raw_headers_list = [shlex.split(raw_line)[1] for raw_line in raw_headers]
-        # Parse "key: value" to [key, value] and filter list if lenght <2 (lost value)
-        # Split method run twice, but that most clearly, than walrus operator in list comprehansion
-        headers_list = [line.split(": ") for line in raw_headers_list if len(line.split(": ")) == 2]
-        headers = dict(headers_list)
-
-        return AuthData(headers=headers, json_data=json_data, params=post_params)
-
-    def dump_authdata(self, filename: str, authfiles_dir: str | Path = "files/auth/") -> None:
-        """Dump from curl data to file.
-
-        Args:
-        ----
-            filename (str): name of the saved file
-            authfiles_dir (str | Path, optional): Dir for saved files. Defaults to "files/auth/".
-
-        Raises:
-        ------
-            DumpAuthFileError: Raw curl content not found in instance.
-        """
-        if self._raw_curl_content:
-            with Path(f"{authfiles_dir}/{filename}").open(mode="w", encoding="utf-8") as fs:
-                fs.write("".join(self._raw_curl_content))
-        else:
-            msg = "Raw curl content not found."
-            raise DumpAuthFileError(msg)
 
     @classmethod
     def read_from_file(cls, curl_file: str | Path) -> Self:
@@ -98,7 +41,7 @@ class AuthDataParser:
 
         Returns:
         -------
-            AuthDataParser: Content of 'curl file'
+            Self(AuthData): AuthData instance.
         """
         if not isinstance(curl_file, (str, Path)):
             msg = "Incorrect type of `curl_file` (str or Path only)"
@@ -128,27 +71,80 @@ class AuthDataParser:
         """
         return [line.replace("^", "") for line in content_with_cr]
 
-    @classmethod
-    def scan_dir(cls, dir_path: str | Path) -> dict[str, AuthData]:
-        """Scan dir to authdata files.
+    def _parse_authdata(self) -> None:
+        """Parse content of curl file to AuthData(headers, data, params) dataclass.
 
         Args:
         ----
-            dir_path (str | Path): path to curl files
+            content (list[str]): content of file 'copy as cURL' from browser
 
         Returns:
         -------
-            dict[str, AuthData]: {filename: AuthData}
+            AuthData: dataclass for store data for authentification on Youtube Music
         """
-        auth_files: dict = {}
-        if isinstance(dir_path, str) and "\\" in dir_path:
-            dir_path = dir_path.replace("\\", "/")
-        dir_path = Path(dir_path)
-        if Path(dir_path).is_dir():
-            files = dir_path.glob("*")
-            for file in files:
-                try:
-                    auth_files |= {file.name: cls.read_from_file(curl_file=file).authdata}
-                except ParsingError:
-                    continue
-        return auth_files
+        # Parsing 'params' section
+        post_url = shlex.split(self.raw_curl_content[0])[1]
+        query_post_url = urlsplit(post_url).query
+        post_params = dict(parse_qsl(query_post_url))
+        # Parsing 'data' section
+        raw_data = shlex.split(s=self.raw_curl_content[-2])[1]
+        json_data = json.loads(s=raw_data)
+        # delete default browse id
+        json_data.pop("browseId", None)
+        # Parsing 'headers' section
+        raw_headers = self.raw_curl_content[1:-2]
+        # Parse str  "-H 'key: value' \\\n" to "key: value"
+        raw_headers_list = [shlex.split(raw_line)[1] for raw_line in raw_headers]
+        # Parse "key: value" to [key, value] and filter list if lenght <2 (lost value)
+        # Split method run twice, but that most clearly, than walrus operator in list comprehansion
+        headers_list = [line.split(": ") for line in raw_headers_list if len(line.split(": ")) == 2]
+        headers = dict(headers_list)
+        self.headers = headers
+        self.params = post_params
+        self.json_data = json_data
+
+
+def search_authfiles_in_dir(dir_path: str | Path) -> dict[str, AuthData]:
+    """Scan dir to authdata files.
+
+    Args:
+    ----
+        dir_path (str | Path): path to curl files
+
+    Returns:
+    -------
+        dict[str, AuthData]: {filename: AuthData}
+    """
+    auth_files: dict = {}
+    if isinstance(dir_path, str) and "\\" in dir_path:
+        dir_path = dir_path.replace("\\", "/")
+    dir_path = Path(dir_path)
+    if Path(dir_path).is_dir():
+        files = dir_path.glob("*")
+        for file in files:
+            try:
+                auth_files |= {file.name: AuthData.read_from_file(curl_file=file)}
+            except ParsingError:
+                continue
+    return auth_files
+
+
+def dump_authdata(authdata: AuthData, filename: str, authfiles_dir: str | Path = "files/auth/") -> None:
+    """Dump from curl data to file.
+
+    Args:
+    ----
+        authdata (AuthData): AuthData instance
+        filename (str): name of the saved file
+        authfiles_dir (str | Path, optional): Dir for saved files. Defaults to "files/auth/".
+
+    Raises:
+    ------
+        DumpAuthFileError: Raw curl content not found in instance.
+    """
+    if authdata.raw_curl_content:
+        with Path(f"{authfiles_dir}/{filename}").open(mode="w", encoding="utf-8") as fs:
+            fs.write("".join(authdata.raw_curl_content))
+    else:
+        msg = "Raw curl content not found."
+        raise DumpAuthFileError(msg)
